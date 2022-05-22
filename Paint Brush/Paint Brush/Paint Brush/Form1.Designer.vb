88fe0afda1f535f878e1a36144bf82384ea21ac3
Daniel Rivaldo <87303588+DanielRivaldo3@users.noreply.github.com>
2022-05-22 22:06:38 +0700

    Add files via upload

---
+++ Paint Brush/Paint Brush/Paint Brush/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PB_draw = New System.Windows.Forms.PictureBox()
+        Me.btn_save = New System.Windows.Forms.Button()
+        Me.btn_clear = New System.Windows.Forms.Button()
+        Me.btn_color = New System.Windows.Forms.Button()
+        Me.cb_size = New System.Windows.Forms.ComboBox()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.PB_draw, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PB_draw
+        '
+        Me.PB_draw.BackColor = System.Drawing.Color.White
+        Me.PB_draw.Location = New System.Drawing.Point(12, 12)
+        Me.PB_draw.Name = "PB_draw"
+        Me.PB_draw.Size = New System.Drawing.Size(373, 344)
+        Me.PB_draw.TabIndex = 0
+        Me.PB_draw.TabStop = False
+        '
+        'btn_save
+        '
+        Me.btn_save.Location = New System.Drawing.Point(391, 12)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(80, 36)
+        Me.btn_save.TabIndex = 1
+        Me.btn_save.Text = "Save (debug folder)"
+        Me.btn_save.UseVisualStyleBackColor = True
+        '
+        'btn_clear
+        '
+        Me.btn_clear.Location = New System.Drawing.Point(391, 54)
+        Me.btn_clear.Name = "btn_clear"
+        Me.btn_clear.Size = New System.Drawing.Size(80, 36)
+        Me.btn_clear.TabIndex = 2
+        Me.btn_clear.Text = "Clear"
+        Me.btn_clear.UseVisualStyleBackColor = True
+        '
+        'btn_color
+        '
+        Me.btn_color.Location = New System.Drawing.Point(391, 96)
+        Me.btn_color.Name = "btn_color"
+        Me.btn_color.Size = New System.Drawing.Size(80, 36)
+        Me.btn_color.TabIndex = 3
+        Me.btn_color.Text = "Color"
+        Me.btn_color.UseVisualStyleBackColor = True
+        '
+        'cb_size
+        '
+        Me.cb_size.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cb_size.FormattingEnabled = True
+        Me.cb_size.Items.AddRange(New Object() {"2", "4", "6", "8", "10"})
+        Me.cb_size.Location = New System.Drawing.Point(391, 138)
+        Me.cb_size.Name = "cb_size"
+        Me.cb_size.Size = New System.Drawing.Size(80, 21)
+        Me.cb_size.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(478, 368)
+        Me.Controls.Add(Me.cb_size)
+        Me.Controls.Add(Me.btn_color)
+        Me.Controls.Add(Me.btn_clear)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.PB_draw)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Paint Brush"
+        CType(Me.PB_draw, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PB_draw As System.Windows.Forms.PictureBox
+    Friend WithEvents btn_save As System.Windows.Forms.Button
+    Friend WithEvents btn_clear As System.Windows.Forms.Button
+    Friend WithEvents btn_color As System.Windows.Forms.Button
+    Friend WithEvents cb_size As System.Windows.Forms.ComboBox
+    Friend WithEvents ColorDialog1 As System.Windows.Forms.ColorDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ Paint Brush/Paint Brush/Paint Brush/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Drawing
+
+Public Class Form1
+
+    Dim draw As Boolean
+    Dim drawcolor As Color = Color.Black
+    Dim drawsize As Integer = 6
+    Dim bmp As Bitmap
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'set initial brush size
+        cb_size.SelectedIndex = 2
+
+        'populate  picturebox image property
+        bmp = New Bitmap(PB_draw.Width, PB_draw.Height)
+        PB_draw.Image = bmp
+
+    End Sub
+
+    Private Sub paintbrush(x As Integer, y As Integer)
+        Using g As Graphics = Graphics.FromImage(PB_draw.Image)
+            g.FillRectangle(New SolidBrush(drawcolor), New Rectangle(x, y, drawsize, drawsize))
+
+        End Using
+        PB_draw.Refresh()
+
+    End Sub
+
+    Private Sub PB_draw_Click(sender As Object, e As EventArgs) Handles PB_draw.Click
+
+    End Sub
+
+    Private Sub PB_draw_MouseDown(sender As Object, e As MouseEventArgs) Handles PB_draw.MouseDown
+        draw = True
+        'first pixel
+        paintbrush(e.X, e.Y)
+    End Sub
+
+    Private Sub PB_draw_MouseMove(sender As Object, e As MouseEventArgs) Handles PB_draw.MouseMove
+        If draw = True Then
+            paintbrush(e.X, e.Y)
+
+        End If
+    End Sub
+
+    Private Sub PB_draw_MouseUp(sender As Object, e As MouseEventArgs) Handles PB_draw.MouseUp
+        draw = False
+    End Sub
+
+    Private Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        bmp = New Bitmap(PB_draw.Width, PB_draw.Height)
+        PB_draw.Image = bmp
+    End Sub
+
+    Private Sub cb_size_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cb_size.SelectedIndexChanged
+        drawsize = cb_size.SelectedItem
+    End Sub
+
+    Private Sub btn_color_Click(sender As Object, e As EventArgs) Handles btn_color.Click
+        If ColorDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            drawcolor = ColorDialog1.color
+        End If
+    End Sub
+
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        PB_draw.DrawToBitmap(bmp, New Rectangle(0, 0, PB_draw.Width, PB_draw.Height))
+        bmp.Save("test2.png", Imaging.ImageFormat.Png)
+
+        bmp = New Bitmap(PB_draw.Width, PB_draw.Height)
+    End Sub
+
+End Class
